f6d6cfae1e123e055e236975f8da02803b078000
ehmicky <ehmicky@gmail.com>
2020-04-12 10:00:10 +0200

    Fix old Node.js support

--- src/path.js
+++ src/path.js
@@ -19,9 +19,12 @@ export const getFilePath = function (cwd) {
 
 const getSearchFiles = function (cwd) {
   const searchDirs = getSearchDirs(cwd)
-  const searchFiles = searchDirs.flatMap((searchDir) =>
-    // eslint-disable-next-line max-nested-callbacks
-    NODE_VERSION_FILES.map((filename) => join(searchDir, filename)),
+  // TODO: replace with Array.flatMap() after dropping support for Node 10
+  const searchFiles = [].concat(
+    ...searchDirs.map((searchDir) =>
+      // eslint-disable-next-line max-nested-callbacks
+      NODE_VERSION_FILES.map((filename) => join(searchDir, filename)),
+    ),
   )
   return searchFiles
 }
