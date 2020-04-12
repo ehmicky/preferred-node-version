395193d2b9cb6df146b6bb2cbf380943bd311778
ehmicky <ehmicky@gmail.com>
2020-04-12 11:59:20 +0200

    Remove debugging code

--- src/main.js
+++ src/main.js
@@ -36,8 +36,6 @@ const findVersion = async function (cwd) {
   return getVersionEnvVariable()
 }
 
-preferredNodeVersion().then(console.log, console.log)
-
 // We do not use `export default` because Babel transpiles it in a way that
 // requires CommonJS users to `require(...).default` instead of `require(...)`.
 module.exports = preferredNodeVersion
