dacc2dd095b92502c25ae00dc04335f5159517fa
ehmicky <ehmicky@gmail.com>
2020-04-12 11:48:27 +0200

    Add more aliases

--- src/alias.js
+++ src/alias.js
@@ -10,10 +10,17 @@ export const getVersionRange = function (rawVersion) {
 }
 
 const ALIASES = {
-  // .nvmrc
-  node: '*',
-  stable: '*',
-  unstable: '0.11',
-  // .naverc
+  // Latest version (nave, nvm-windows, n, nvs, nodebrew, nodist, fish-nvm)
   latest: '*',
+  // Latest version (nvm, nave, nodebrew)
+  stable: '*',
+  // Latest version (nvm)
+  node: '*',
+  // Latest version (n, fish-nvm)
+  current: '*',
+  // Alias from nvm. Now that iojs is merged to Node.js, it is always this
+  // version.
+  iojs: '4.0.0',
+  // Old deprecated nvm alias
+  unstable: '0.11',
 }
