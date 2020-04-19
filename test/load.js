137e1c6178debe4f1fda883e963e8867c9eabe65
ehmicky <ehmicky@gmail.com>
2020-04-19 23:26:14 +0200

    Refactor tests

--- test/load.js
+++ test/load.js
@@ -4,13 +4,9 @@ import { each } from 'test-each'
 import { runFixture } from './helpers/main.js'
 import { TEST_VERSION } from './helpers/versions.js'
 
-test('Trim version file content', async (t) => {
-  const { version } = await runFixture('trim')
-  t.is(version, TEST_VERSION)
-})
-
 each(
   [
+    'trim',
     'naverc',
     'node-version',
     'n-node-version',
