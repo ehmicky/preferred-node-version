1c2afb2b12f0ebe3aee29950186f879d6af5f56d
ehmicky <ehmicky@gmail.com>
2020-04-19 23:28:28 +0200

    Add more tests

--- test/helpers/versions.js
+++ test/helpers/versions.js
@@ -1,1 +1,3 @@
 export const TEST_VERSION = '6.0.0'
+export const ALIAS_VERSION = '6.17.1'
+export const RESOLVED_VERSION_RANGE = '6.17.1'
--- test/main.js
+++ test/main.js
@@ -3,7 +3,18 @@ import isPlainObj from 'is-plain-obj'
 
 import preferredNodeVersion from '../src/main.js'
 
-import { setHomeDir, unsetHomeDir } from './helpers/main.js'
+import { runFixture, setHomeDir, unsetHomeDir } from './helpers/main.js'
+import { ALIAS_VERSION, RESOLVED_VERSION_RANGE } from './helpers/versions.js'
+
+test('Resolves aliases', async (t) => {
+  const { version } = await runFixture('alias')
+  t.is(version, ALIAS_VERSION)
+})
+
+test('Resolves version ranges', async (t) => {
+  const { version } = await runFixture('version_range')
+  t.is(version, RESOLVED_VERSION_RANGE)
+})
 
 test.serial('Returns an empty object if nothing was found', async (t) => {
   setHomeDir('/')
