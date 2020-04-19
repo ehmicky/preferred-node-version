9387a20808f303d2c1b3ed28c2c7bb074594abd6
ehmicky <ehmicky@gmail.com>
2020-04-19 23:19:42 +0200

    Add more tests

--- test/path.js
+++ test/path.js
@@ -1,3 +1,6 @@
+import { promises as fs } from 'fs'
+import { platform } from 'process'
+
 import test from 'ava'
 
 import preferredNodeVersion from '../src/main.js'
@@ -13,13 +16,30 @@ test('Prioritize closer directories', async (t) => {
 })
 
 test('Ignore version directories', async (t) => {
-  const cwd = `${FIXTURES_DIR}/directory`
+  const cwd = `${FIXTURES_DIR}/directory/subdir`
   const { version } = await preferredNodeVersion({ cwd })
   t.is(version, TEST_VERSION)
 })
 
 test('Ignore invalid version files', async (t) => {
-  const cwd = `${FIXTURES_DIR}/ignore_invalid`
+  const cwd = `${FIXTURES_DIR}/ignore_invalid/subdir`
   const { version } = await preferredNodeVersion({ cwd })
   t.is(version, TEST_VERSION)
 })
+
+if (platform !== 'win32') {
+  test.serial('Ignore version files with wrong permissions', async (t) => {
+    const cwd = `${FIXTURES_DIR}/ignore_permissions/subdir`
+    const nvmrcPath = `${cwd}/.nvmrc`
+
+    const { mode } = await fs.stat(nvmrcPath)
+    await fs.chmod(nvmrcPath, 0)
+
+    try {
+      const { version } = await preferredNodeVersion({ cwd })
+      t.is(version, TEST_VERSION)
+    } finally {
+      await fs.chmod(nvmrcPath, mode)
+    }
+  })
+}
